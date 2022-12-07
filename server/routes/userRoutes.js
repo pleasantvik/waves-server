ffc854a74c6820188b08e0a000ce0ab8f91c27e2
pleasantvik <adedayo.ayoola5683@gmail.com>
2022-12-07 07:37:39 +0100

    completed the user auth flow

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -12,8 +12,15 @@ const router = express.Router();
 router.post("/signup", authController.signup);
 router.post("/signin", authController.signin);
 router.get("/isauth", authController.isauth);
-router.get("/forgotPassword", authController.forgotPassword);
-router.get("/resetPassword", authController.resetPassword);
+router.post("/forgotPassword", authController.forgotPassword);
+router.patch("/resetPassword/:token", authController.resetPassword);
+router.patch(
+  "/updateMyPassword",
+  authController.protect,
+  authController.updatePassword
+);
+router.patch("/updateMe", authController.protect, userController.updateMe);
+router.delete("/deleteMe", authController.protect, userController.deleteMe);
 
 router
   .route("/")
