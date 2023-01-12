e1526067fd7c36f6d21687af5b5f06a6861c90d1
pleasantvik <adedayo.ayoola5683@gmail.com>
2023-01-12 09:15:10 +0100

    live site test

--- server/app.js
+++ server/app.js
@@ -42,11 +42,11 @@ app.use("/api/site", siteRouter);
 // Unhandled Route
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../client/build"));
+  app.use(express.static("client/build"));
   const path = require("path");
   app.get("/*", (req, res) => {
-    // res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-    res.sendFile(`../client/build/index.html`);
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+    // res.sendFile(`../client/build/index.html`);
   });
 }
 
